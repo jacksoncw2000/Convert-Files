55d46c323571b51d42bcf81f8e911c2e6fd8d677
Jackson Williams <78338284+jacksoncw2000@users.noreply.github.com>
2023-12-08 22:18:32 -0600

    Update m4a_to_mp3.py

--- m4a_to_mp3.py
+++ m4a_to_mp3.py
@@ -1,7 +1,7 @@
 from pydub import AudioSegment
 
 # Load the m4a file
-m4a_audio = AudioSegment.from_file("/Users/jackson/Desktop/Avery Chinese.m4a", format="m4a")
+m4a_audio = AudioSegment.from_file("file_path.m4a", format="m4a")
 
 # Convert to mp3
-m4a_audio.export("TEST 1.mp3", format="mp3")
+m4a_audio.export("export_file_name.mp3", format="mp3")
