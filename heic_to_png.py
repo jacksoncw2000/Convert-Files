386dbdeb941b5e44572f2105ed244e944d85fc7f
Jackson Williams <78338284+jacksoncw2000@users.noreply.github.com>
2023-12-08 22:17:45 -0600

    Update heic_to_png.py

--- heic_to_png.py
+++ heic_to_png.py
@@ -4,7 +4,7 @@ def convert_heic_to_png(heic_file_path, png_file_path):
     subprocess.run(["magick", heic_file_path, png_file_path])
 
 # Example usage
-heic_file_path = '/Users/jackson/Desktop/IMG_6401.HEIC'
-png_file_path = 'Headshot Jackson Williams.png'
+heic_file_path = '.HEIC'
+png_file_path = '.png'
 
 convert_heic_to_png(heic_file_path, png_file_path)
