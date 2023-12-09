e35e91ec690f0419e11bf264ac65b0801d1dbef2
Jackson Williams <78338284+jacksoncw2000@users.noreply.github.com>
2023-12-08 22:19:49 -0600

    Update heic_to_png.py

--- heic_to_png.py
+++ heic_to_png.py
@@ -5,6 +5,6 @@ def convert_heic_to_png(heic_file_path, png_file_path):
 
 # Example usage
 heic_file_path = 'file_path.HEIC'
-png_file_path = 'file_path.png'
+png_file_path = 'export_file_name.png'
 
 convert_heic_to_png(heic_file_path, png_file_path)
