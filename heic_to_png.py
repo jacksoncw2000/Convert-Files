37b34e13947242e10220cdb6eaf917462e0073f9
Jackson Williams <78338284+jacksoncw2000@users.noreply.github.com>
2023-12-08 22:28:12 -0600

    Update heic_to_png.py

--- heic_to_png.py
+++ heic_to_png.py
@@ -1,10 +1,4 @@
-import os
 import subprocess
-import datetime
-from PIL import Image
-from pillow_heif import register_heif_opener
-from pathlib import Path
-from tqdm import tqdm
 
 def convert_heic_to_png(heic_file_path, png_file_path):
     subprocess.run(["magick", heic_file_path, png_file_path])
