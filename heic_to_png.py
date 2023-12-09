ba2c49c7c8f0d6b4afc13db3bdb23dd96d7cbc13
Jackson Williams <78338284+jacksoncw2000@users.noreply.github.com>
2023-12-08 22:19:04 -0600

    Update heic_to_png.py

--- heic_to_png.py
+++ heic_to_png.py
@@ -4,7 +4,7 @@ def convert_heic_to_png(heic_file_path, png_file_path):
     subprocess.run(["magick", heic_file_path, png_file_path])
 
 # Example usage
-heic_file_path = '.HEIC'
-png_file_path = '.png'
+heic_file_path = 'file_path.HEIC'
+png_file_path = 'file_path.png'
 
 convert_heic_to_png(heic_file_path, png_file_path)
